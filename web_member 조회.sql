6cfa9be87021500dc720958d13cbc0e883090b41
hayoungishere <48154977+hayoungishere@users.noreply.github.com>
2019-07-13 14:03:01 +0900

    Create web_member 조회.sql

---
+++ web_member 조회.sql
@@ -0,0 +1,5 @@
+select * from web_member;
+
+insert into web_member(id, pwd, name, address, phone_number, age,join_dt, admin_yn) 
+values('admin','1234','admin','hwagok','010-1234-1234','99',sysdate, 'Y');
+
